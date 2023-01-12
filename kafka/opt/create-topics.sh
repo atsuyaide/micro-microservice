a473d5494ced93f7d519f58441a47935ec2c5eff
Atsuya Ide <atsuya.ide528@gmail.com>
2023-01-13 00:43:15 +0900

    add: KSQL

--- kafka/opt/create-topics.sh
+++ kafka/opt/create-topics.sh
@@ -2,6 +2,9 @@
 
 kafka-topics --bootstrap-server kafka:9092 --list
 
-# 追加
+echo 'Creating kafka topics'
 kafka-topics --bootstrap-server kafka:9092 --create --topic topic-01 --partitions 3 replication-factor 1
 kafka-topics --bootstrap-server kafka:9092 --create --topic topic-02 --partitions 3 replication-factor 1
+
+echo 'Successfully created the following topics:'
+kafka-topics --bootstrap-server kafka:9092 --list
